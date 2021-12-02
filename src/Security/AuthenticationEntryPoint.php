d6c7ea4f32e303a105c97a37f96105057e059b4e
GonzalvoJustine <justine.gonzalvo@outlook.fr>
2021-12-02 17:17:28 +0100

    update sass and bootstrap for each exercises in the session

--- src/Security/AuthenticationEntryPoint.php
+++ src/Security/AuthenticationEntryPoint.php
@@ -20,7 +20,7 @@ class AuthenticationEntryPoint implements AuthenticationEntryPointInterface
     public function start(Request $request, AuthenticationException $authException = null): RedirectResponse
     {
         // add a custom flash message and redirect to the login page
-        $request->getSession()->getFlashBag()->add('note', 'You have to login in order to access this page.');
+        $request->getSession()->getFlashBag()->add('note', 'Vous devez vous identifier pour accéder à cette page.');
 
         return new RedirectResponse($this->urlGenerator->generate('security_login'));
     }
